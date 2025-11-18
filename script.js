3a74e19862838e6c0d43fabbcc837426ec73b958
Angust1408 <godanticandy0.0@gmail.com>
2025-11-18 19:11:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ const audio = document.getElementById("audio");
 const refresh = document.getElementById("refresh");
 
 playBtn.addEventListener("click", () => {
+    // Ẩn nút play, hiện ảnh scare
     playBtn.style.display = "none";
     scareImg.style.display = "block";
 
     // Fullscreen
     if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen().catch(err => console.log(err));
+        document.documentElement.requestFullscreen();
     }
 
-    // Play 1 lần (file đã nối 2 lần)
-    audio.src = "scream2.mp3";
-    audio.play().catch(err => console.log("Audio không phát được:", err));
+    // Audio
+    audio.loop = true;
+    audio.play();
 
     // Nhấp nháy nền
     let isWhite = false;
-    const flashInterval = setInterval(() => {
+    setInterval(() => {
         document.body.style.background = isWhite ? "black" : "white";
         isWhite = !isWhite;
-    }, 200);
+    }, 200); // 200ms dịu mắt hơn
 
     // Hiện chữ haha sau 2 giây
-    setTimeout(() => refresh.style.display = "block", 2000);
-
-    // Dừng nhấp nháy khi audio kết thúc
-    audio.onended = () => clearInterval(flashInterval);
+    setTimeout(() => {
+        refresh.style.display = "block";
+    }, 2000);
 });
